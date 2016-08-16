7df61b9d099a9c3c249a42fd09c3e9f25b7ace8c
Val <vgeyvandov@gmail.com>
2016-08-16 13:00:49 -0400

    file type and file size limitations

--- routes/api.js
+++ routes/api.js
@@ -1,43 +1,62 @@
 var express = require('express');
 var multer  = require('multer');
 var router = express.Router();
-var upload = multer();
 var s3 = require('../helpers/s3');
 var utils = require('../helpers/utils');
 var db = require('../helpers/db');
 var imagePost = require('../helpers/image-post');
 
 
-/* GET home page. */
-router.post('/api/upload', upload.single('image'), function (req, res) {
-	var imageFile = req.file;
-	var body = req.body;
-	var uniqueFileName = utils.generateUniqueName(imageFile.originalname);
-	
-	var params = {
-        Key: uniqueFileName,
-        Body: imageFile.buffer
-    };
+var upload = multer({
+    limits: { fileSize: 5000000 },
+    fileFilter: function (req, file, cb) {
+        var mimeTypes = ['image/png', 'image/jpeg'];
 
-    var imagePostObj = imagePost.generate({
-    	name: uniqueFileName,
-    	title: body.title
-    });
+        if (mimeTypes.indexOf(file.mimetype) < 0) {
+            return cb(new Error('Unsupported file type! :O'), false);
+        }
 
-    s3.getBucket().putObject(params, function (error, response) {
-    	var message;
-        if (error) {
-        	message = 'Error uploading data';
+        cb(null, true);
+    }
+}).single('image');
+
+
+/* POST image. */
+router.post('/api/upload', function (req, res) {
+
+    upload(req, res, function (err) {
+        var imageFile = req.file;
+
+        if (imageFile) {
+            var body = req.body;
+            var uniqueFileName = utils.generateUniqueName(imageFile.originalname);
+            
+            var params = {
+                Key: uniqueFileName,
+                Body: imageFile.buffer
+            };
+
+            var imagePostObj = imagePost.generate({
+                name: uniqueFileName,
+                title: body.title
+            });
+
+            s3.getBucket().putObject(params, function (error, response) {
+                
+                if (error) {
+                    res.json({ message: 'Error uploading image! :O' });
+                } else {
+                    db.get().collection('imagePosts').insert(imagePostObj, function (err, confirmation) {
+                        if (err) { res.json({ message: 'Uh Oh, something went wrong! :O' }); }
+                        res.json({ message: 'Successfully uploaded data! :D' });
+                    });
+                }
+
+            });
         } else {
-        	message = 'Successfully uploaded data';
-        	db.get().collection('imagePosts').insert(imagePostObj, function(err, confirmation) {
-        		res.json({
-		        	message: message
-				});
-        	});
+            res.json({ message: err.message });
         }
     });
-
 	
 });
 

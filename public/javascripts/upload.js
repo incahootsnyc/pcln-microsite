39d63edf8e279681ede21fab6b6dc89a72ecda60
Tanya <tanya.brassie@utexas.edu>
2016-08-22 14:54:00 -0400

    Modal CSS/HTML

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -107,10 +107,12 @@ pclnPicMe.uploadModalDelegate = (function () {
 	function addCloseEventFn ($uploadModal) {
 
 		var $uploadClose = $uploadModal.find('#upload-modal-close');
+		var $overlay = $('#overlay');
 
 
 			$uploadClose.click(function(){
 				$uploadModal.hide();
+				$overlay.hide();
 			});
 	}
 	

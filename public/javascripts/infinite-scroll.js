2e02d967763fb255662756766a811900cfaafcb4
Val <vgeyvandov@gmail.com>
2016-08-23 16:02:35 -0400

    fixing dynamic li html

--- public/javascripts/infinite-scroll.js
+++ public/javascripts/infinite-scroll.js
@@ -35,7 +35,8 @@
 		var itemHTML = '<li class="submissions__img-container">' + 
 					'<img class="submissions__img" data-id="' + imagePostObject.uniqueName + '" src="' + imagePostObject.thumbUrl + '">' + 
 		 			'<div class="submission__info">' + 
-		 				'<span class="submissions__username"><a href="">Val Geyvandvos</a></span> <span class="submissions__likes">Likes:' + imagePostObject.likesCount + '</span>' +
+		 				'<span class="submissions__username"><a href="">Val Geyvandvos</a></span>' +
+		 				'<span class="submissions__likes"><img class="submissions__like-icon" src="/images/like.svg"/>Likes:' + imagePostObject.likesCount + '</span>' +
 					'</div>' + 
 				'</li>';
 

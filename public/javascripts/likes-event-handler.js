192494596ef5e4fab1ef34c6bbfc54b87e3e77c1
Val <vgeyvandov@gmail.com>
2016-08-24 14:25:41 -0400

    fixing likes selector

--- public/javascripts/likes-event-handler.js
+++ public/javascripts/likes-event-handler.js
@@ -1,6 +1,6 @@
 pclnPicMe.likesEventHandler = (function () {
 
-	$('#image-list').on('click', '.submissions__like-icon', function () {
+	$('#image-list').on('click', '.submissions__icon-container', function () {
 		var $_this = $(this);
 		var postId = $_this.closest('.submissions__img-container').find('.submissions__img').data('id');
 		registerLike(postId, $_this.siblings('.like-value'));

6512a823eedaeb3847f62e4ab2c0eea62770dba1
Tanya <tanya.brassie@utexas.edu>
2016-08-25 18:26:50 -0400

    sexy responsive nav fixes amongst other things.

--- public/javascripts/header.js
+++ public/javascripts/header.js
@@ -11,29 +11,39 @@ var menuHidden = true;
 
 window.onresize = function() {	
 	var screenWidth = document.documentElement.clientWidth;	  	
+	
 	if (screenWidth > 840) {
 	  	hamburgerMenu.css('display', 'none');
-	  	mobileMenu.css('display', 'none');
+	  	mobileMenu.css('right', '-320px');
 	  	hamburgerMenu.attr('src', 'images/hamburger.png');
 	  	menuHidden = true;
+
 	
 	} else {
+
   		hamburgerMenu.css('display', 'block');
+
+  		// menuHidden = true;
+  		// mobileMenu.css('display', 'block')
   	}
 };  	
 
 
-hamburgerMenu.click(function (){
-	console.log("hello");
+hamburgerMenu.click(function() {
+  	if (menuHidden) {
+  		hamburgerMenu.attr('src', 'images/menu-close.png');
+      	mobileMenu.animate({
+	      right: 0
+	    }, 800 );
 
-	if (menuHidden) {
-		mobileMenu.toggle();
-		hamburgerMenu.attr('src', 'images/menu-close.png');
-		menuHidden = false;
-	} else {
-		mobileMenu.toggle();
-		hamburgerMenu.attr('src', 'images/hamburger.png');
-
-		menuHidden = true;
-	}	
+	    menuHidden = !menuHidden;
+  	} else {
+  		hamburgerMenu.attr('src', 'images/hamburger.png');
+    	mobileMenu.animate({
+    	right: "-320px"
+    	}, 800 );
+   	 	menuHidden = !menuHidden;
+  	}
 });
+
+

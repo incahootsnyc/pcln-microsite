66a3b4f0e103a916ea8bb1c2aebd1f87a689fc6b
Tanya <tanya.brassie@utexas.edu>
2016-08-28 13:00:41 -0400

    Mobile Nav menu working finally as I would like it. Fuck yeh! High Fives! Glee!

--- public/javascripts/header.js
+++ public/javascripts/header.js
@@ -1,54 +1,44 @@
 (function () {
-  var hamburgerMenu = $('#hamburger-icon');
-  var mobileMenu = $('#navbar-mobile');
+  var $hamburgerMenu = $('#hamburger-icon');
+  var $mobileMenu = $('#navbar-mobile');
   var menuHidden = true;
 
 
   // window.onload = function() { 
   //  var screenWidth = document.documentElement.clientWidth;           
   //    if (screenWidth > 850) {
-  //      hamburgerMenu.css('display', 'none');
+  //      $hamburgerMenu.css('display', 'none');
   //    }
 
   window.onresize = function() {  
     var screenWidth = document.documentElement.clientWidth;     
     
     if (screenWidth > 840) {
-        hamburgerMenu.css('display', 'none');
-        //mobileMenu.css('right', '-320px');
-        mobileMenu.removeClass('slideLeft');
-        hamburgerMenu.attr('src', 'images/hamburger.png');
-        menuHidden = true;
+        $hamburgerMenu.css('display', 'none').attr('src', 'images/hamburger.png');
+        $mobileMenu.css('display', 'none').removeClass('slideInAnimation');
+        menuHidden = true;    
 
     
     } else {
 
-        hamburgerMenu.css('display', 'block');
-
-        // menuHidden = true;
-        // mobileMenu.css('display', 'block')
+        $hamburgerMenu.css('display', 'block');
+        $mobileMenu.css('display', 'block')
       }
   };    
 
 
-  hamburgerMenu.click(function() {
+  $hamburgerMenu.click(function() {
       if (menuHidden) {
 
-      mobileMenu.addClass("slideLeft");
-      hamburgerMenu.attr('src', 'images/menu-close.png');
-        //   mobileMenu.animate({
-        //   right: 0, opacity: 1
-        // }, 100 );
+      $mobileMenu.addClass("slideInAnimation");
+      $hamburgerMenu.attr('src', 'images/menu-close.png');
 
-        menuHidden = !menuHidden;
+      menuHidden = !menuHidden;
+
       } else {
-        mobileMenu.removeClass("slideLeft");
-      	// mobileMenu.removeClass("menu-enter");
-      	// mobileMenu.addClass("menu-exit");
-         hamburgerMenu.attr('src', 'images/hamburger.png');
-        // mobileMenu.animate({
-        // right: "-250px", opacity: 0
-        // }, 100 );
+        $mobileMenu.removeClass("slideInAnimation");
+         $hamburgerMenu.attr('src', 'images/hamburger.png');
+
         menuHidden = !menuHidden;
       }
   });
